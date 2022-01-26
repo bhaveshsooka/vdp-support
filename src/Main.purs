3946bcd4fabe75f70a988b068f2f2fcdfbdd4308
Bhavesh Sooka <bsooka@gmail.com>
2022-01-26 16:12:43 +0200

    Code cleanup

--- src/Main.purs
+++ src/Main.purs
@@ -8,13 +8,3 @@ import Test.Routing (routingWidget)
 
 main :: Effect Unit
 main = runWidgetInDom "root" routingWidget
-  -- $ orr
-  --   [ widget routingWidget "Routing"
-  --   ]
-  -- where
-  --   widget w s =
-  --     orr
-  --       [ D.hr'
-  --       , D.h2_ [] $ D.text s
-  --       , D.div_ [] w
-  --       ]

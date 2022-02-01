22f5255fcfbd212d1b24c4d35513ac0fd76c9273
Bhavesh Sooka <bsooka@gmail.com>
2022-02-01 13:45:54 +0200

    Consumer restarts content

--- src/Pages/OperationsPage/OperationsPage.purs
+++ src/Pages/OperationsPage/OperationsPage.purs
@@ -9,7 +9,7 @@ import Concur.React.DOM as D
 import Concur.React.Props as P
 import Control.Alt ((<|>))
 import Test.Pages.OperationsPage.OperationsPageTypes (MyTab(..), MyTabAction(..))
-import Test.Styles (operationsTopbarItemStyle, operationsTopbarStyle)
+import Test.Styles (consumerRestartsButtonGroupsStyle, consumerRestartsButtonStyle, consumerRestartsContentStyle, operationsTopbarItemStyle, operationsTopbarStyle)
 
 operationsPage :: forall a. MyTab -> MyTabAction -> Widget HTML a
 operationsPage activeTab action = do
@@ -33,6 +33,39 @@ operationsPage activeTab action = do
           $> HealthChecks
       ]
       <|> case activeTab of
-          ConsumerRestarts -> D.div' [ D.text "Consumer Restarts Content" ]
+          ConsumerRestarts -> consumerRestartsContent
           HealthChecks -> D.div' [ D.text "Health Checks Content" ]
   operationsPage selectedTab action
+
+type ConsumerServiceInfo
+  = { marketName :: String
+    , pauseEndpoint :: String
+    , restartEndpoint :: String
+    }
+
+consumerServices :: Array ConsumerServiceInfo
+consumerServices =
+  [ { marketName: "service1", pauseEndpoint: "http:service1/pause", restartEndpoint: "http:service1/restart" }
+  , { marketName: "service2", pauseEndpoint: "http:service2/pause", restartEndpoint: "http:service2/restart" }
+  , { marketName: "service3", pauseEndpoint: "http:service3/pause", restartEndpoint: "http:service3/restart" }
+  , { marketName: "service4", pauseEndpoint: "http:service4/pause", restartEndpoint: "http:service4/restart" }
+  , { marketName: "service5", pauseEndpoint: "http:service5/pause", restartEndpoint: "http:service5/restart" }
+  , { marketName: "service6", pauseEndpoint: "http:service6/pause", restartEndpoint: "http:service6/restart" }
+  , { marketName: "service7", pauseEndpoint: "http:service7/pause", restartEndpoint: "http:service7/restart" }
+  , { marketName: "service8", pauseEndpoint: "http:service8/pause", restartEndpoint: "http:service8/restart" }
+  , { marketName: "service9", pauseEndpoint: "http:service9/pause", restartEndpoint: "http:service9/restart" }
+  ]
+
+consumerRestartsContent :: forall a. Widget HTML a
+consumerRestartsContent =
+  D.div
+    [ consumerRestartsContentStyle ]
+    (map buttonGroups consumerServices)
+  where
+  buttonGroups :: forall b. ConsumerServiceInfo -> Widget HTML b
+  buttonGroups consumerService =
+    D.div [ consumerRestartsButtonGroupsStyle ]
+      [ D.h3' [ D.text consumerService.marketName ]
+      , D.button [ consumerRestartsButtonStyle "#aa0441" ] [ D.text "Pause" ]
+      , D.button [ consumerRestartsButtonStyle "#04AA6D" ] [ D.text "Restart" ]
+      ]
--- src/Styles.purs
+++ src/Styles.purs
@@ -1,5 +1,8 @@
 module Test.Styles
-  ( operationsTopbarItemStyle
+  ( consumerRestartsButtonGroupsStyle
+  , consumerRestartsButtonStyle
+  , consumerRestartsContentStyle
+  , operationsTopbarItemStyle
   , operationsTopbarStyle
   , sidebarItemStyle
   , sidebarStyle
@@ -21,7 +24,7 @@ sidebarStyle =
     , "left": "0"
     , "background-color": "#111"
     , "overflow-x": "hidden"
-    , "padding-top": "20px"
+    , "padding-top": "10px"
     }
 
 sidebarItemStyle :: String -> forall a. P.ReactProps a
@@ -39,6 +42,7 @@ operationsTopbarStyle =
   P.style
     { "background-color": "#333"
     , "overflow": "hidden"
+    , "margin-bottom": "10px"
     }
 
 operationsTopbarItemStyle :: forall a. Boolean -> Boolean -> P.ReactProps a
@@ -53,5 +57,41 @@ operationsTopbarItemStyle active hover =
     , "color": setColor active "white" $ setColor hover "black" "#f2f2f2"
     }
 
+consumerRestartsContentStyle :: forall a. P.ReactProps a
+consumerRestartsContentStyle =
+  P.style
+    { "display": "flex"
+    , "flex-wrap": "wrap"
+    }
+
+consumerRestartsButtonGroupsStyle :: forall a. P.ReactProps a
+consumerRestartsButtonGroupsStyle =
+  P.style
+    { "flex-grow": "1"
+    , "width": "20%"
+    , "text-align": "center"
+    , "margin": "10px 10px"
+    , "padding": "10px 0px"
+    , "border-style": "double"
+    }
+
+consumerRestartsButtonStyle :: forall a. String -> P.ReactProps a
+consumerRestartsButtonStyle color =
+  P.style
+    { "background-color": color
+    -- , "border": "none"
+    , "color": "white"
+    , "padding": "20px"
+    , "text-align": "center"
+    , "text-decoration": "none"
+    , "display": "inline-block"
+    , "font-size": "16px"
+    , "margin": "4px 2px"
+    , "border-radius": "50%"
+    }
+
+--------------------------------------------------------------------------------------------------
+-- helper functions
+--------------------------------------------------------------------------------------------------
 setColor :: Boolean -> String -> String -> String
 setColor active colTrue colFalse = if (active == true) then colTrue else colFalse

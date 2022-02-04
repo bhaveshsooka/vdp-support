363d966d426332718e4820e5527d59e7248c72fa
Bhavesh Sooka <bsooka@gmail.com>
2022-02-04 15:22:43 +0200

    help page types added

---
+++ web-ui/src/Pages/HelpPage/HelpPageTypes.purs
@@ -0,0 +1,26 @@
+module Test.Pages.HelpPage.HelpPageTypes
+  ( MyTab(..)
+  , MyTabAction(..)
+  ) where
+
+import Prelude
+
+data MyTab
+  = ArchitectureDiagrams
+  | Walkthroughs
+
+instance eqMyTab :: Eq MyTab where
+  eq ArchitectureDiagrams ArchitectureDiagrams = true
+  eq Walkthroughs Walkthroughs = true
+  eq _ _ = false
+
+data MyTabAction
+  = Click
+  | Hover
+  | Unhover
+
+instance eqMyTabAction :: Eq MyTabAction where
+  eq Click Click = true
+  eq Hover Hover = true
+  eq Unhover Unhover = true
+  eq _ _ = false

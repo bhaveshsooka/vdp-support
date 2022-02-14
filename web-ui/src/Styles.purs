e6ba57be159e24c3e86bf7b7046f5b69d8730171
Bhavesh Sooka <bsooka@gmail.com>
2022-02-14 22:43:44 +0200

    camelCase style props

--- web-ui/src/Styles.purs
+++ web-ui/src/Styles.purs
@@ -17,41 +17,41 @@ sidebarStyle =
     { "width": "15.0%"
     , "height": "100%"
     , "position": "fixed"
-    , "z-index": "1"
+    , "zIndex": "1"
     , "top": "0"
     , "left": "0"
-    , "background-color": "#111"
-    , "overflow-x": "hidden"
-    , "padding-top": "10px"
+    , "backgroundColor": "#111"
+    , "overflowX": "hidden"
+    , "paddingTop": "10px"
     }
 
 sidebarItemStyle :: String -> forall a. P.ReactProps a
 sidebarItemStyle fontSize =
   P.style
     { "padding": "6px 8px 6px 16px"
-    , "text-decoration": "none"
+    , "textDecoration": "none"
     , "color": "#818181"
     , "display": "block"
-    , "font-size": fontSize
+    , "fontSize": fontSize
     }
 
 topbarStyle :: forall a. P.ReactProps a
 topbarStyle =
   P.style
-    { "background-color": "#333"
+    { "backgroundColor": "#333"
     , "overflow": "hidden"
-    , "margin-bottom": "10px"
+    , "marginBottom": "10px"
     }
 
 topbarItemStyle :: forall a. Boolean -> Boolean -> P.ReactProps a
 topbarItemStyle active hover =
   P.style
     { "float": "left"
-    , "text-align": "center"
+    , "textAlign": "center"
     , "padding": "14px 16px"
-    , "text-decoration": "none"
-    , "font-size": "17px"
-    , "background-color": setColor active "#04AA6D" $ setColor hover "#ddd" "#333"
+    , "textDecoration": "none"
+    , "fontSize": "17px"
+    , "backgroundColor": setColor active "#04AA6D" $ setColor hover "#ddd" "#333"
     , "color": setColor active "white" $ setColor hover "black" "#f2f2f2"
     }
 
@@ -59,33 +59,32 @@ operationsContentStyle :: forall a. P.ReactProps a
 operationsContentStyle =
   P.style
     { "display": "flex"
-    , "flex-wrap": "wrap"
+    , "flexWrap": "wrap"
     }
 
 operationsButtonGroupsStyle :: forall a. P.ReactProps a
 operationsButtonGroupsStyle =
   P.style
-    { "flex-grow": "1"
+    { "flexGrow": "1"
     , "width": "20%"
-    , "text-align": "center"
+    , "textAlign": "center"
     , "margin": "10px 10px"
     , "padding": "10px 0px"
-    , "border-style": "double"
+    , "borderStyle": "double"
     }
 
 operationsButtonStyle :: forall a. String -> P.ReactProps a
 operationsButtonStyle color =
   P.style
-    { "background-color": color
-    -- , "border": "none"
+    { "backgroundColor": color
     , "color": "white"
     , "padding": "20px"
-    , "text-align": "center"
-    , "text-decoration": "none"
+    , "textAlign": "center"
+    , "textDecoration": "none"
     , "display": "inline-block"
-    , "font-size": "16px"
+    , "fontSize": "16px"
     , "margin": "4px 2px"
-    , "border-radius": "50%"
+    , "borderRadius": "50%"
     }
 
 --------------------------------------------------------------------------------------------------

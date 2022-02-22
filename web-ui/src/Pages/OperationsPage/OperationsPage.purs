139b5d29a9b26bf983cbcfa0587043e52f228478
Bhavesh Sooka <bsooka@gmail.com>
2022-02-22 16:30:24 +0200

    http requests working

--- web-ui/src/Pages/OperationsPage/OperationsPage.purs
+++ web-ui/src/Pages/OperationsPage/OperationsPage.purs
@@ -3,14 +3,15 @@ module VDPSupport.Pages.OperationsPage
   ) where
 
 import Prelude
+import Affjax as AX
+import Affjax.ResponseFormat as ResponseFormat
 import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.React.DOM as D
-import Data.Argonaut.Core (Json)
-import Data.Argonaut.Decode (class DecodeJson, JsonDecodeError, decodeJson, (.:))
+import Data.Argonaut.Decode (class DecodeJson, decodeJson, (.:))
 import Data.Array (cons)
-import Data.Either (Either)
-import Data.Traversable (traverse)
+import Data.Either (Either(..))
+import Effect.Aff.Class (liftAff)
 import VDPSupport.Styles (operationsButtonGroupsStyle, operationsButtonStyle, operationsContentStyle)
 import VDPSupport.Topbar (TopbarAction(..), TopbarItem(..), TopbarItemArray, findActiveTab, getTopbarItem, topbarWidget, updateTabItems)
 
@@ -65,27 +66,20 @@ instance decodeJsonConsumerServiceInfo :: DecodeJson ConsumerServiceInfo where
     re <- obj .: "restartEndpoint"
     pure $ ConsumerServiceInfo { marketName: mn, pauseEndpoint: pe, restartEndpoint: re }
 
-decodeConsumerServiceInfoArray :: Json -> Either JsonDecodeError ConsumerServiceInfoArray
-decodeConsumerServiceInfoArray json = decodeJson json >>= traverse decodeJson
-
-consumerServices :: Array ConsumerServiceInfo
-consumerServices =
-  [ ConsumerServiceInfo { marketName: "market1", pauseEndpoint: "http:service1/pause", restartEndpoint: "http:service1/restart" }
-  , ConsumerServiceInfo { marketName: "market2", pauseEndpoint: "http:service2/pause", restartEndpoint: "http:service2/restart" }
-  , ConsumerServiceInfo { marketName: "market3", pauseEndpoint: "http:service3/pause", restartEndpoint: "http:service3/restart" }
-  , ConsumerServiceInfo { marketName: "market4", pauseEndpoint: "http:service4/pause", restartEndpoint: "http:service4/restart" }
-  , ConsumerServiceInfo { marketName: "market5", pauseEndpoint: "http:service5/pause", restartEndpoint: "http:service5/restart" }
-  , ConsumerServiceInfo { marketName: "market6", pauseEndpoint: "http:service6/pause", restartEndpoint: "http:service6/restart" }
-  , ConsumerServiceInfo { marketName: "market7", pauseEndpoint: "http:service7/pause", restartEndpoint: "http:service7/restart" }
-  , ConsumerServiceInfo { marketName: "market8", pauseEndpoint: "http:service8/pause", restartEndpoint: "http:service8/restart" }
-  , ConsumerServiceInfo { marketName: "market9", pauseEndpoint: "http:service9/pause", restartEndpoint: "http:service9/restart" }
-  ]
-
-consumerRestartsContent :: forall a. Widget HTML a
-consumerRestartsContent =
-  D.div
-    [ operationsContentStyle ]
-    (map buttonGroups consumerServices)
+consumerRestartsContent ∷ forall a. Widget HTML a
+consumerRestartsContent = do
+  let
+    url = "http://localhost:3000/vdp-services/consumers"
+  res <- liftAff $ AX.get ResponseFormat.json url
+  case res of
+    Left err -> do
+      D.div' [ D.text $ "GET " <> url <> " There was a problem making the request: " <> AX.printError err ]
+    Right response -> do
+      case decodeJson response.body of
+        Right (r :: ConsumerServiceInfoArray) -> do
+          D.div [ operationsContentStyle ] (map buttonGroups r)
+        Left e -> do
+          D.div' [ D.text $ "Can't parse JSON. : " <> show e ]
   where
   buttonGroups :: forall b. ConsumerServiceInfo -> Widget HTML b
   buttonGroups (ConsumerServiceInfo consumerService) =
@@ -95,71 +89,36 @@ consumerRestartsContent =
       , D.button [ operationsButtonStyle "#04AA6D" ] [ D.text "Restart" ]
       ]
 
--- showData = (map buttonGroups fetchData)
--- fetchData = do
---   let
---     url = "http://localhost:3000/vdp-services/consumers"
---   httpResult <- (liftAff (AX.get ResponseFormat.json url)) <|> (D.text "Loading...")
---   case httpResult of
---     Left err -> D.text $ "GET " <> url <> " response failed: " <> AX.printError err
---     Right response ->
---       case (decodeConsumerServiceInfoArray response.body) of
---         Left err -> D.text ("Decode json error: " <> show err)
---         Right consumerServices -> map buttonGroups consumerServices
--- fetchData
--- fetchReddit :: forall a. String -> Widget HTML a
--- fetchReddit sub =
---   div'
---     [ h4' [ text ("/r/" <> sub) ]
---     , showPosts
---     ]
---   where
---   -- showPosts = button [onClick] [text "Fetch posts"] >>= \_ -> fetchPosts
---   fetchPosts = do
---     let
---       url = "http://localhost:3000/vdp-services/consumers"
---     result <- (liftAff (AX.get ResponseFormat.json url)) <|> (D.text "Loading...")
---     case result of
---       Left err -> D.text $ "GET " <> url <> " response failed to decode: " <> AX.printError err
---       Right response -> do
---         let
---           postsResp = do
---             o <- decodeJson response.body
---             d1 <- o .: "data"
---             cs <- d1 .: "children"
---             decodeConsumerServiceInfoArray cs
---         case postsResp of
---           Left err -> D.text ("Error: " <> show err)
---           Right posts -> do
---             D.div'
---               [ D.div' (map (\(Post p) -> div' [ text p.title ]) (take 5 posts))
---               , D.div' [ button [ unit <$ onClick ] [ text "Refresh" ] ]
---               ]
---             fetchPosts
 newtype HealthCheckInfo
   = HealthCheckInfo
   { serviceName :: String
   , healthCheckEndpoint :: String
   }
 
-healthCheckServices :: Array HealthCheckInfo
-healthCheckServices =
-  [ HealthCheckInfo { serviceName: "service1", healthCheckEndpoint: "http:service1/health" }
-  , HealthCheckInfo { serviceName: "service2", healthCheckEndpoint: "http:service2/health" }
-  , HealthCheckInfo { serviceName: "service3", healthCheckEndpoint: "http:service3/health" }
-  , HealthCheckInfo { serviceName: "service4", healthCheckEndpoint: "http:service4/health" }
-  , HealthCheckInfo { serviceName: "service5", healthCheckEndpoint: "http:service5/health" }
-  , HealthCheckInfo { serviceName: "service6", healthCheckEndpoint: "http:service6/health" }
-  , HealthCheckInfo { serviceName: "service7", healthCheckEndpoint: "http:service7/health" }
-  , HealthCheckInfo { serviceName: "service8", healthCheckEndpoint: "http:service8/health" }
-  , HealthCheckInfo { serviceName: "service9", healthCheckEndpoint: "http:service9/health" }
-  ]
+type HealthCheckInfoArray
+  = Array HealthCheckInfo
+
+instance decodeJsonHealthCheckInfo :: DecodeJson HealthCheckInfo where
+  decodeJson json = do
+    obj <- decodeJson json
+    sn <- obj .: "serviceName"
+    hce <- obj .: "healthCheckEndpoint"
+    pure $ HealthCheckInfo { serviceName: sn, healthCheckEndpoint: hce }
 
 healthCheckContent :: forall a. Widget HTML a
-healthCheckContent =
-  D.div
-    [ operationsContentStyle ]
-    (map buttonGroups healthCheckServices)
+healthCheckContent = do
+  let
+    url = "http://localhost:3000/vdp-services/health-check-services"
+  res <- liftAff $ AX.get ResponseFormat.json url
+  case res of
+    Left err -> do
+      D.div' [ D.text $ "GET " <> url <> " There was a problem making the request: " <> AX.printError err ]
+    Right response -> do
+      case decodeJson response.body of
+        Right (r :: HealthCheckInfoArray) -> do
+          D.div [ operationsContentStyle ] (map buttonGroups r)
+        Left e -> do
+          D.div' [ D.text $ "Can't parse JSON. : " <> show e ]
   where
   buttonGroups :: forall b. HealthCheckInfo -> Widget HTML b
   buttonGroups (HealthCheckInfo consumerService) =
